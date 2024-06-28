9bf0c38216662317dc86f9edfe2091c87d6f3d15
sumanth <jellasumanth1999@gmail.com>
2024-06-28 23:48:57 +0530

    Added Entities, Repositories and Services for : Task, User

--- src/main/java/com/pesto/tech/taskManagement/service/UserService.java
+++ src/main/java/com/pesto/tech/taskManagement/service/UserService.java
@@ -3,27 +3,22 @@ package com.pesto.tech.taskManagement.service;
 import com.pesto.tech.taskManagement.entity.User;
 import com.pesto.tech.taskManagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.Collection;
-import java.util.List;
-
 public class UserService implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private UserRepository theUserRepository;
 
     @Autowired
     public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+        this.theUserRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User theUser = userRepository.loadUserByUsername(username);
+        User theUser = theUserRepository.loadUserByUsername(username);
         UserDetails theUserDetails = org.springframework.security.core.userdetails.User.builder()
                 .username(theUser.getUserName()).password(theUser.getPassword())
                 .accountExpired(false).build();
