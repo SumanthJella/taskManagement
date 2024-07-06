d2b9674d4973e39b3e78e20bae90230a6d21595e
sumanth <jellasumanth1999@gmail.com>
2024-07-06 23:42:38 +0530

    Security Config

---
+++ src/main/java/com/pesto/tech/taskManagement/security/JwtSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.pesto.tech.taskManagement.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+
+@Configuration
+public class JwtSecurityConfig {
+
+    //AuthenticationManagerBuilder
+}
